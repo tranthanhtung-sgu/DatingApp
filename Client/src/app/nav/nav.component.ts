1cf5f3ab89275d9cd221fb7c5354198b877d5d26
Trần Thanh Tùng <50178152+tranthanhtung-sgu@users.noreply.github.com>
2021-02-03 23:59:10 +0700

    Routing Angular

--- Client/src/app/nav/nav.component.ts
+++ Client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -14,7 +16,8 @@ export class NavComponent implements OnInit {
 
   currentUser$: Observable<User>;
 
-  constructor(private accountService : AccountService) { }
+  constructor(private accountService : AccountService, private router: Router,
+    private toastr: ToastrService) { }
 
   ngOnInit(): void {
     this.currentUser$ = this.accountService.currentUser$;
@@ -22,14 +25,16 @@ export class NavComponent implements OnInit {
 
   login() {
     this.accountService.login(this.model).subscribe(response=>{
-      console.log(response);
-    }, errors => {
-      console.log(errors);
-      
+      this.toastr.success("Đăng nhập thành công")
+      this.router.navigateByUrl('/members');
+    }, error => {
+      this.toastr.error(error.error)
+      console.log(error);
     })
   }
 
   logout() {
+    this.router.navigateByUrl('/');
     this.accountService.logout();
   }
 
